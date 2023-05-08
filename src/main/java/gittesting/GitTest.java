c5d1e25720699ae8d226a337c4bfe0f24757e2e2
Mehmet Duran Kaya <mehmetdurankaya@gmail.com>
2023-05-08 14:20:59 +0300

    Uygulama v-1.2 olarak güncellendi.Bir mısra eklendi ki.....

--- src/main/java/gittesting/GitTest.java
+++ src/main/java/gittesting/GitTest.java
@@ -5,9 +5,13 @@ public class GitTest {
 
         //Yunus Emre divanından bir dörtlük seçildi.Kendi nefsime hazırlanıp "VKS" ile takip amaçlandı.
         String digit="İlim ilim bilmektir.";
+        String friends="İlim kendin bilmektir.";
 
         System.out.println(digit);
-        System.out.println("Uygulama v-1.1 için yayınlandı!");
+        System.out.println("Uygulama v-1.1 olarak yayınlandı!");
         System.out.println("***********************************");
+        //v-1.2 çalışmaları başladı...
+        System.out.println(friends);
+        System.out.println("Uygulama v-1.2 olarak güncellendi.Bir mısra eklendi ki.....");
     }
 }

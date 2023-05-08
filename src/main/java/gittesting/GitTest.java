9bac0eae385ee37bdb81a25ac9431ab6ed7ea4df
Mehmet Duran Kaya <mehmetdurankaya@gmail.com>
2023-05-08 14:08:20 +0300

    Uygulama v-1.1 olaray yayınlandı.

---
+++ src/main/java/gittesting/GitTest.java
@@ -0,0 +1,13 @@
+package gittesting;
+
+public class GitTest {
+    public static void main(String[] args) {
+
+        //Yunus Emre divanından bir dörtlük seçildi.Kendi nefsime hazırlanıp "VKS" ile takip amaçlandı.
+        String digit="İlim ilim bilmektir.";
+
+        System.out.println(digit);
+        System.out.println("Uygulama v-1.1 için yayınlandı!");
+        System.out.println("***********************************");
+    }
+}
